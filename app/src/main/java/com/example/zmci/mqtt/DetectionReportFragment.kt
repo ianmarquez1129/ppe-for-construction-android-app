82b1671bf16a5b4599d11982f27fe85ffa179b3b
ianmarquez1129 <ianmarquez1129@gmail.com>
2023-06-03 21:48:04 +0800

    feat: add clickable image in Detection Report and image screen maximization

--- app/src/main/java/com/example/zmci/mqtt/DetectionReportFragment.kt
+++ app/src/main/java/com/example/zmci/mqtt/DetectionReportFragment.kt
@@ -1,18 +1,31 @@
 package com.example.zmci.mqtt
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Point
+import android.graphics.Rect
+import android.graphics.RectF
 import android.os.Bundle
 import android.util.Base64
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import androidx.fragment.app.Fragment
 import com.example.zmci.R
 import kotlinx.android.synthetic.main.fragment_detection_report.*
 import org.json.JSONArray
 
+
 class DetectionReportFragment : Fragment() {
+
+    private var currentAnimator: Animator? = null
+    private var shortAnimationDuration: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +50,12 @@ class DetectionReportFragment : Fragment() {
         val decodedByte = Base64.decode(image, Base64.DEFAULT)
         val bitmap = BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.size)
         imageReport.setImageBitmap(bitmap)
+
+
+        imageReport.setOnClickListener {
+            zoomImageFromThumb(thumbView = imageReport,bitmap)
+        }
+        shortAnimationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
 
         cameraReport.text = "Camera details: \n"
         timestampReport.text = "Timestamp: $timestamp"
@@ -103,6 +122,137 @@ class DetectionReportFragment : Fragment() {
             }
         } catch (e:Exception) {
             e.printStackTrace()
+        }
+    }
+
+    private fun zoomImageFromThumb(thumbView: View, imageResId: Bitmap) {
+        // If there's an animation in progress, cancel it immediately and
+        // proceed with this one.
+        currentAnimator?.cancel()
+
+        // Load the high-resolution "zoomed-in" image.
+        expandedImage.setImageBitmap(imageResId)
+
+        // Calculate the starting and ending bounds for the zoomed-in image.
+        val startBoundsInt = Rect()
+        val finalBoundsInt = Rect()
+        val globalOffset = Point()
+
+        // The start bounds are the global visible rectangle of the thumbnail,
+        // and the final bounds are the global visible rectangle of the
+        // container view. Set the container view's offset as the origin for the
+        // bounds, since that's the origin for the positioning animation
+        // properties (X, Y).
+        thumbView.getGlobalVisibleRect(startBoundsInt)
+        container.getGlobalVisibleRect(finalBoundsInt, globalOffset)
+        startBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+        finalBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+
+        val startBounds = RectF(startBoundsInt)
+        val finalBounds = RectF(finalBoundsInt)
+
+        // Using the "center crop" technique, adjust the start bounds to be the
+        // same aspect ratio as the final bounds. This prevents unwanted
+        // stretching during the animation. Calculate the start scaling factor.
+        // The end scaling factor is always 1.0.
+        val startScale: Float
+        if ((finalBounds.width() / finalBounds.height() > startBounds.width() / startBounds.height())) {
+            // Extend start bounds horizontally.
+            startScale = startBounds.height() / finalBounds.height()
+            val startWidth: Float = startScale * finalBounds.width()
+            val deltaWidth: Float = (startWidth - startBounds.width()) / 2
+            startBounds.left -= deltaWidth.toInt()
+            startBounds.right += deltaWidth.toInt()
+        } else {
+            // Extend start bounds vertically.
+            startScale = startBounds.width() / finalBounds.width()
+            val startHeight: Float = startScale * finalBounds.height()
+            val deltaHeight: Float = (startHeight - startBounds.height()) / 2f
+            startBounds.top -= deltaHeight.toInt()
+            startBounds.bottom += deltaHeight.toInt()
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation
+        // begins, it positions the zoomed-in view in the place of the
+        // thumbnail.
+        thumbView.alpha = 0f
+
+        animateZoomToLargeImage(startBounds, finalBounds, startScale)
+
+        setDismissLargeImageAnimation(thumbView, startBounds, startScale)
+    }
+
+    private fun animateZoomToLargeImage(startBounds: RectF, finalBounds: RectF, startScale: Float) {
+        expandedImage.visibility = View.VISIBLE
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations to the
+        // top-left corner of the zoomed-in view. The default is the center of
+        // the view.
+        expandedImage.pivotX = 0f
+        expandedImage.pivotY = 0f
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties: X, Y, SCALE_X, and SCALE_Y.
+        currentAnimator = AnimatorSet().apply {
+            play(
+                ObjectAnimator.ofFloat(
+                    expandedImage,
+                    View.X,
+                    startBounds.left,
+                    finalBounds.left)
+            ).apply {
+                with(ObjectAnimator.ofFloat(expandedImage, View.Y, startBounds.top, finalBounds.top))
+                with(ObjectAnimator.ofFloat(expandedImage, View.SCALE_X, startScale, 1f))
+                with(ObjectAnimator.ofFloat(expandedImage, View.SCALE_Y, startScale, 1f))
+            }
+            duration = shortAnimationDuration.toLong()
+            interpolator = DecelerateInterpolator()
+            addListener(object : AnimatorListenerAdapter() {
+
+                override fun onAnimationEnd(animation: Animator) {
+                    currentAnimator = null
+                }
+
+                override fun onAnimationCancel(animation: Animator) {
+                    currentAnimator = null
+                }
+            })
+            start()
+        }
+    }
+
+    private fun setDismissLargeImageAnimation(thumbView: View, startBounds: RectF, startScale: Float) {
+        // When the zoomed-in image is tapped, it zooms down to the original
+        // bounds and shows the thumbnail instead of the expanded image.
+        expandedImage.setOnClickListener {
+            currentAnimator?.cancel()
+
+            // Animate the four positioning and sizing properties in parallel,
+            // back to their original values.
+            currentAnimator = AnimatorSet().apply {
+                play(ObjectAnimator.ofFloat(expandedImage, View.X, startBounds.left)).apply {
+                    with(ObjectAnimator.ofFloat(expandedImage, View.Y, startBounds.top))
+                    with(ObjectAnimator.ofFloat(expandedImage, View.SCALE_X, startScale))
+                    with(ObjectAnimator.ofFloat(expandedImage, View.SCALE_Y, startScale))
+                }
+                duration = shortAnimationDuration.toLong()
+                interpolator = DecelerateInterpolator()
+                addListener(object : AnimatorListenerAdapter() {
+
+                    override fun onAnimationEnd(animation: Animator) {
+                        thumbView.alpha = 1f
+                        expandedImage.visibility = View.GONE
+                        currentAnimator = null
+                    }
+
+                    override fun onAnimationCancel(animation: Animator) {
+                        thumbView.alpha = 1f
+                        expandedImage.visibility = View.GONE
+                        currentAnimator = null
+                    }
+                })
+                start()
+            }
         }
     }
 }
